39634e7750b2d332dae6f274a666c8e8426c6325
Anton Libeman <antonliberman@gmail.com>
2016-08-17 15:45:01 +0300

    rewrite

--- src/tel_ran/pictures/model/dao/PicturesMap.java
+++ src/tel_ran/pictures/model/dao/PicturesMap.java
@@ -6,15 +6,21 @@ import java.util.*;
 
 public class PicturesMap implements IPictures {
 	
+	private static final float DEFAULT_SIMILARITY = 0.5f;
 	private Map<String,Set<String>> tagsPictures;// key - tag, value - set of url's pictures of the tag
 	private Map<String,Picture> pictures;// key - url, value - picture object reference
+	private float similarity = DEFAULT_SIMILARITY;
 	
 	public PicturesMap() {
 		super();
 		this.tagsPictures = new HashMap<String,Set<String>>();
 		this.pictures = new HashMap<String,Picture>();
 	}
-
+	public PicturesMap(float similarity){
+		this();
+		this.similarity = similarity;
+	}
+	
 	@Override
 	public boolean addPicture(Picture picture) {
 		String url = picture.getUrl();
@@ -25,50 +31,48 @@ public class PicturesMap implements IPictures {
 		for(String tag : tags){
 			Set<String> urlsOfTag = tagsPictures.get(tag);
 			if(urlsOfTag == null){
-				Set<String> setOfUrls = new HashSet<String>();
-				setOfUrls.add(url);
-				tagsPictures.put(tag, setOfUrls);
-			} else{
-				urlsOfTag.add(url);
+				urlsOfTag = new HashSet<String>();
+				tagsPictures.put(tag, urlsOfTag);
 			}
+			urlsOfTag.add(url);
 		}
 		return true;
 	}
 
 	@Override
 	public Iterable<Picture> findSimilar(Picture picture) {
-		// >= 50 % of tags is same
-		HashMap<String, Integer> res = new HashMap<String, Integer>();
+		
 		String[] tagsOfPicture = picture.getTags();
+		int thresholdSim = (int) (similarity  * tagsOfPicture.length + 0.5f);
+		HashMap<String, Integer> urlCount = new HashMap<String, Integer>();
+		ArrayList<Picture> listRes = new ArrayList<Picture>();
+
 		for(String tag : tagsOfPicture){
 			Set<String> urls = tagsPictures.get(tag);
 			if(urls != null)
-				addUrlsToRes(urls,res);
+				addUrlsToUrlCount(urls,urlCount,thresholdSim,listRes);
 		}
-		ArrayList<Picture> list = new ArrayList<Picture>();
-		Set<Map.Entry<String,Integer>> set = res.entrySet();
-		Integer similaryConst = (int) (0.5 * tagsOfPicture.length);
-		for(Map.Entry<String,Integer> entry : set){
-			if(entry.getValue() >= similaryConst){
-				list.add(pictures.get(entry.getKey()));
-			}
-		}
-		return list;
+		return listRes;
 	}
 
-	private void addUrlsToRes(Set<String> urls, HashMap<String, Integer> res) {
+	private void addUrlsToUrlCount(Set<String> urls, HashMap<String, Integer> urlCount, int thresholdSim,ArrayList<Picture> listRes) {
 		for(String url : urls){
-			Integer counter = res.get(url);
-			if(counter == null)
-				res.put(url, 1);
-			else 
-				res.put(url, ++counter);
+			Integer counter = urlCount.getOrDefault(url,0);
+			counter++;
+				
+			if(counter >= thresholdSim){
+				listRes.add(pictures.get(url));
+				counter = -1000;
+			}
+			urlCount.put(url, counter);
+			
 		}
 	}
 
 	@Override
 	public Picture getPicture(String url) {
-		
+		if(url == null)
+			return null;
 		return pictures.get(url);
 	}
 
